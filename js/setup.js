b0074c83752b4a9b99bc006bcdb28b1a8b94dda4
vladimir-rurik <36802394+vladimir-rurik@users.noreply.github.com>
2018-06-17 11:26:36 +0300

    основные сценарии взаимодействия пользователя с сайтом

--- js/setup.js
+++ js/setup.js
@@ -1,68 +1,270 @@
 'use strict';
 
-(function () {
+var FIRST_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
 
-  var WIZARD_COUNT = 4;
-  var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var LAST_NAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
 
-  var userDialog = document.querySelector('.setup');
-  userDialog.classList.remove('hidden');
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
 
-  var similarListElement = userDialog.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+var EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 
-  /** функция выбора случайного элемента массива
-   * @param {Array} arr - исходный массив
-   * @return {string} arr[random]
-   */
-  var randomArrItem = function (arr) {
-    var rand = Math.floor(Math.random() * arr.length);
-    return arr[rand];
+var FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+/**
+ * Количество похожих персонажей
+ * @const
+ * @type {number}
+ */
+var SIMILAR_WIZARDS_COUNT = 4;
+
+/**
+ * Код клавиши Escape
+ * @const
+ * @type {number}
+ */
+var ESC_KEYCODE = 27;
+
+/**
+ * Код клавиши Enter
+ * @const
+ * @type {number}
+ */
+var ENTER_KEYCODE = 13;
+
+var setupWindow = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setupWindow.querySelector('.setup-close');
+var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
+var setupUserName = setupWindow.querySelector('.setup-user-name');
+var setupWizard = setupWindow.querySelector('.setup-wizard');
+var setupWizardEyes = setupWizard.querySelector('.wizard-eyes');
+var eyesColorField = setupWindow.querySelector('[name=eyes-color]');
+var setupFireball = setupWindow.querySelector('.setup-fireball-wrap');
+var fireballColorField = setupFireball.querySelector('[name=fireball-color]');
+var setupSimilar = setupWindow.querySelector('.setup-similar');
+var similarWizardsList = setupWindow.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var eyesColorValue = eyesColorField.value;
+var eyesColorIndex = EYES_COLORS.indexOf(eyesColorValue);
+var fireballColorValue = fireballColorField.value;
+var fireballColorIndex = FIREBALL_COLORS.indexOf(fireballColorValue);
+
+var similarWizards = [];
+
+/**
+ * Функция, выбирающая случайный элемент в массиве.
+ * @param {Array.<*>} items - массив элементов
+ * @return {*} - случайный элемент массива
+ */
+var getRandomItem = function (items) {
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+/**
+ * Функция, генерирующая похожего персонажа случайным образом.
+ * @return {Object} - JS объект, описывающий персонажа
+ */
+var generateRandomWizard = function () {
+  var firstName = getRandomItem(FIRST_NAMES);
+  var lastName = getRandomItem(LAST_NAMES);
+  var fullName = getRandomItem([0, 1]) ? firstName + ' ' + lastName : lastName + ' ' + firstName;
+
+  return {
+    name: fullName,
+    coatColor: getRandomItem(COAT_COLORS),
+    eyesColor: getRandomItem(EYES_COLORS)
   };
+};
 
-  /** функция возвращающая массив волшебников
-   * @param {number} wizardCount - кол-во волшебников
-   * @return {Array} wizards
-   */
-  var getWizards = function (wizardCount) {
-    var wizards = [];
+/**
+ * Функция, создающая массив персонажей.
+ * @param {number} length - длина массива
+ * @return {Array.<Object>}
+ */
+var getWizardsList = function (length) {
+  var dataList = [];
+  for (var i = 0; i < length; i++) {
+    dataList[i] = generateRandomWizard();
+  }
 
-    for (var i = 0; i < wizardCount; i++) {
-      wizards[i] =
-        {
-          name: randomArrItem(WIZARD_FIRST_NAMES) + ' ' + randomArrItem(WIZARD_LAST_NAMES),
-          coatColor: randomArrItem(coatColor),
-          eyesColor: randomArrItem(eyesColor)
-        };
-    }
-    return wizards;
-  };
+  return dataList;
+};
 
-  /** функция заполнения DOM объекта свойствами объекта волшебника
-   * @param {Wizard} wizard - объект исходного волшебника
-   * @return {DOMobject} wizardElement
-   */
-  var fillWizard = function (wizard) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
+/**
+ * Функция, создающая DOM-элемент, соответствующй похожему персонажу.
+ * @callback renderItemCallback
+ * @param {Object} wizard - объект, описывающий похожего персонажа
+ * @param {Object} wizardTemplate - шаблон похожего персонажа
+ * @return {Object} - DOM-элемент
+ */
+var renderWizard = function (wizard, wizardTemplate) {
+  var wizardElement = wizardTemplate.cloneNode(true);
 
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-    return wizardElement;
-  };
+  return wizardElement;
+};
 
+/**
+ * Функция, отрисовывающая массив DOM-элементов.
+ * @param {Array.<Object>} dataList - массив объектов, содержащий данные элементов
+ * @param {Object} parentElement - родительский DOM-элемент, в котором будут отрисованы элементы
+ * @param {Object} template - шаблон элемента
+ * @param {renderItemCallback} renderItem - функция, создающая DOM-элемент
+ */
+var renderElements = function (dataList, parentElement, template, renderItem) {
   var fragment = document.createDocumentFragment();
+  for (var i = 0; i < dataList.length; i++) {
+    fragment.appendChild(renderItem(dataList[i], template));
+  }
+  parentElement.appendChild(fragment);
+};
 
-  getWizards(WIZARD_COUNT).forEach(function (wizard) {
-    fragment.appendChild(fillWizard(wizard));
-  });
+/**
+ * Функция, показывающая окно настройки персонажа.
+ */
+var openSetupWindow = function () {
+  setupWindow.classList.remove('hidden');
 
-  similarListElement.appendChild(fragment);
+  setupOpen.removeEventListener('click', openSetupClickHandler);
+  setupOpenIcon.removeEventListener('keydown', openSetupEnterPressHandler);
 
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  document.addEventListener('keydown', setupWindowEscPressHandler);
+};
 
-})();
+/**
+ * Функция, скрывающая окно настройки персонажа.
+ */
+var closeSetupWindow = function () {
+  setupWindow.classList.add('hidden');
+
+  setupOpen.addEventListener('click', openSetupClickHandler);
+  setupOpenIcon.addEventListener('keydown', openSetupEnterPressHandler);
+
+  document.removeEventListener('keydown', setupWindowEscPressHandler);
+};
+
+/**
+ * Функция, определяющая следующий индекс элемента массива по кругу.
+ * @param {Array.<*>} items - массив элементов
+ * @param {number} currentIndex - индекс текущего элемента
+ * @return {number} - индекс следующего элемента (по кругу)
+ */
+var getNextIndex = function (items, currentIndex) {
+  return ++currentIndex % items.length;
+};
+
+/**
+ * Функция, меняющая цвет глаз по порядку.
+ */
+var changeEyesColor = function () {
+  eyesColorIndex = getNextIndex(EYES_COLORS, eyesColorIndex);
+  eyesColorValue = EYES_COLORS[eyesColorIndex];
+
+  setupWizardEyes.style.fill = eyesColorValue;
+  eyesColorField.value = eyesColorValue;
+};
+
+/**
+ * Функция, меняющая цвет фаербола по порядку.
+ */
+var changeFireballColor = function () {
+  fireballColorIndex = getNextIndex(FIREBALL_COLORS, fireballColorIndex);
+  fireballColorValue = FIREBALL_COLORS[fireballColorIndex];
+
+  setupFireball.style.background = fireballColorValue;
+  fireballColorField.value = fireballColorValue;
+};
+
+/**
+ * Обработчик клика по аватарке
+ */
+var openSetupClickHandler = function () {
+  openSetupWindow();
+};
+
+/**
+ * Обработчик нажатия клавиши ENTER на аватарке
+ */
+var openSetupEnterPressHandler = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetupWindow();
+  }
+};
+
+/**
+ * Обработчик нажатия клавиши ESC при открытом окне настроек.
+ */
+var setupWindowEscPressHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && evt.target !== setupUserName) {
+    closeSetupWindow();
+  }
+};
+
+similarWizards = getWizardsList(SIMILAR_WIZARDS_COUNT);
+renderElements(similarWizards, similarWizardsList, similarWizardTemplate, renderWizard);
+setupSimilar.classList.remove('hidden');
+
+// Открытие окна настроек
+setupOpen.addEventListener('click', openSetupClickHandler);
+setupOpenIcon.addEventListener('keydown', openSetupEnterPressHandler);
+
+// Закрытие окна настроек
+setupClose.addEventListener('click', function () {
+  closeSetupWindow();
+});
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetupWindow();
+  }
+});
+
+// Выбор цвета глаз
+setupWizardEyes.addEventListener('click', function () {
+  changeEyesColor();
+});
+
+// Выбор цвета фаербола
+setupFireball.addEventListener('click', function () {
+  changeFireballColor();
+});
--- js/stat.js
+++ js/stat.js
@@ -1,114 +1,281 @@
 'use strict';
 
-(function () {
+/**
+ * Ширина облака
+ * @const
+ * @type {number}
+ */
+var CLOUD_WIDTH = 420;
 
-  // размеры облака
-  var CLOUD_WIDTH = 420;
-  var CLOUD_HEIGHT = 270;
-  // координаты верхней левой точки облака
-  var CLOUD_X = 100;
-  var CLOUD_Y = 10;
-  // величина смещения
-  var GAP = 10;
+/**
+ * Высота облака
+ * @const
+ * @type {number}
+ */
+var CLOUD_HEIGHT = 270;
 
-  var SHADOW_COLOR = 'rgba(0, 0, 0, 0.3)';
-  var TEXT_FONT = '16px PT Mono';
+/**
+ * X-координата левого верхнего угла облака
+ * @const
+ * @type {number}
+ */
+var CLOUD_X = 100;
 
-  // параметры гистограммы
-  var BAR_WIDTH = 40;
-  var BAR_HEIGHT = 150;
-  var BAR_GAP = 50;
-  var USER_DEFAULT_COLOR = 'rgba(255, 0, 0, 1)'; // red
-  var OTHER_DEFAULT_COLOR_TEMPLATE = 'rgba(0, 0, 255, {0})'; // blue with replacable opacity
-  var USER_NAME = 'Вы';
+/**
+ * Y-координата левого верхнего угла облака
+ * @const
+ * @type {number}
+ */
+var CLOUD_Y = 10;
 
-  var COLUMN_BLOCK_X = CLOUD_X + GAP * 4;
-  var COLUMN_BLOCK_Y = CLOUD_Y + GAP * 11 + BAR_HEIGHT;
+/**
+ * Сдвиг тени от облака по горизонтали и вертикали
+ * @const
+ * @type {number}
+ */
+var CLOUD_SHIFT = 10;
 
+/**
+ * Размер отклонения середин сторон облака
+ * @const
+ * @type {number}
+ */
+var CLOUD_DEFLECTION = 18;
 
-  /** функция отрисовки облака
-   * @param {Canvas} ctx
-   * @param {number} x - координата левого угла
-   * @param {number} y - координата верхнего угла
-   * @param {Color} color
-   */
-  var renderCloud = function (ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  };
+/**
+ * Параметры шрифта облака
+ * @const
+ * @type {string}
+ */
+var CLOUD_TEXT_STYLE = '16px PT Mono';
 
-  /** функция нахождения максимального значения
-   * @param {Array} arr - исходный массив
-   * @return {number} максимальный элемент
-   */
-  var getMaxValue = function (arr) {
-    var MaxValue = arr[0];
+/**
+ * Высота текстовых строк
+ * @const
+ * @type {number}
+ */
+var TEXT_HEIGHT = 20;
 
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] > MaxValue) {
-        MaxValue = arr[i];
-      }
+/**
+ * Максимальная высота колонки гистограммы
+ * @const
+ * @type {number}
+ */
+var BAR_MAX_HEIGHT = 150;
+
+/**
+ * Ширина колонки гистограммы
+ * @const
+ * @type {number}
+ */
+var BAR_WIDTH = 40;
+
+/**
+ * Расстояние между колонками гистограммы
+ * @const
+ * @type {number}
+ */
+var BAR_MARGIN = 50;
+
+/**
+ * Минимальная насыщенность цвета синей колонки
+ * @const
+ * @type {number}
+ */
+var MIN_SATURATION = 25;
+
+/**
+ * Максимальная насыщенность цвета синей колонки
+ * @const
+ * @type {number}
+ */
+var MAX_SATURATION = 100;
+
+/**
+ * Минимальная светлота цвета синей колонки
+ * @const
+ * @type {number}
+ */
+var MIN_LIGHTNESS = 30;
+
+/**
+ * Максимальная светлота цвета синей колонки
+ * @const
+ * @type {number}
+ */
+var MAX_LIGHTNESS = 70;
+
+/**
+ * Цвет облака
+ * @const
+ * @type {string}
+ */
+var CLOUD_COLOR = 'rgba(255, 255, 255, 1)';
+
+/**
+ * Цвет тени облака
+ * @const
+ * @type {string}
+ */
+var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+
+/**
+ * Цвет текста облака
+ * @const
+ * @type {string}
+ */
+var TEXT_COLOR = 'rgba(0, 0, 0, 1)';
+
+/**
+ * Цвет колонки пользователя 'Вы'
+ * @const
+ * @type {string}
+ */
+var USER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+
+/**
+ * Функция, отрисовывающая облако.
+ * @param {Object} ctx - контекст отрисовки
+ * @param {number} startX - x-координата левого верхнего угла
+ * @param {number} startY - y-координата левого верхнего угла
+ * @param {number} width - габаритная ширина облака
+ * @param {number} height - габаритная высота облака
+ * @param {string} color - цвет облака
+ */
+var renderCloud = function (ctx, startX, startY, width, height, color) {
+  // координаты правого нижнего угла облака
+  var endX = startX + width;
+  var endY = startY + height;
+
+  // точки излома
+  var points = [
+    {
+      x: startX + width * 0.7,
+      y: startY + CLOUD_DEFLECTION
+    },
+    {
+      x: endX - CLOUD_DEFLECTION,
+      y: startY + height * 0.4
+    },
+    {
+      x: startX + width * 0.6,
+      y: endY - CLOUD_DEFLECTION
+    },
+    {
+      x: startX + CLOUD_DEFLECTION,
+      y: startY + height * 0.5
     }
+  ];
 
-    return MaxValue;
-  };
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(points[0].x, points[0].y);
+  ctx.bezierCurveTo(points[0].x, points[0].y, startX + width * 0.75, startY, startX + width * 0.85, startY);
+  ctx.bezierCurveTo(startX + width * 0.95, startY, endX, startY + height * 0.1, endX, startY + height * 0.2);
+  ctx.bezierCurveTo(endX, startY + height * 0.3, points[1].x, points[1].y, points[1].x, points[1].y);
+  ctx.bezierCurveTo(points[1].x, points[1].y, endX, startY + height * 0.5, endX, startY + height * 0.7);
+  ctx.bezierCurveTo(endX, startY + height * 0.9, startX + width * 0.95, endY, startX + width * 0.8, endY);
+  ctx.bezierCurveTo(startX + width * 0.65, endY, points[2].x, points[2].y, points[2].x, points[2].y);
+  ctx.bezierCurveTo(points[2].x, points[2].y, startX + width * 0.5, endY, startX + width * 0.3, endY);
+  ctx.bezierCurveTo(startX + width * 0.1, endY, startX, startY + height * 0.95, startX, startY + height * 0.75);
+  ctx.bezierCurveTo(startX, startY + height * 0.55, points[3].x, points[3].y, points[3].x, points[3].y);
+  ctx.bezierCurveTo(points[3].x, points[3].y, startX, startY + height * 0.4, startX, startY + height * 0.2);
+  ctx.bezierCurveTo(startX, startY, startX + width * 0.15, startY, startX + width * 0.4, startY);
+  ctx.bezierCurveTo(startX + width * 0.65, startY, points[0].x, points[0].y, points[0].x, points[0].y);
+  ctx.closePath();
+  ctx.fill();
+};
 
-  /** функция отрисовки строки приветствия
-  * @param {Canvas} ctx
-  * @param {string} text
-  * @param {number} x - координата левого угла
-  * @param {number} y - координата верхнего угла
-  */
-  var writeText = function (ctx, text, x, y) {
-    ctx.fillStyle = '#000';
-    ctx.font = TEXT_FONT;
-    ctx.fillText(text, x, y);
-  };
+/**
+ * Функция, определяющая наибольший элемент в массиве.
+ * @param {Array.<number>} items - числовой массив
+ * @return {number} - наибольшее значение в массиве
+ */
+var findMaxItem = function (items) {
+  var maxItem = items[0];
 
-  /** функция получения цвета с рандомной прозрачностью
-   * @param {Array} names
-   * @param {number} index
-   * @return {string} bar color
-   */
-  var getBarColor = function (names, index) {
-    return names[index] === USER_NAME ? USER_DEFAULT_COLOR : OTHER_DEFAULT_COLOR_TEMPLATE.replace('{0}', Math.random().toFixed(2));
-  };
-
-  /** функция отрисовки гистограммы
-   * @param {Canvas} ctx
-   * @param {Array} names
-   * @param {number} index
-   * @param {number} x
-   * @param {number} height
-   */
-  var renderColumn = function (ctx, names, index, x, height) {
-    ctx.fillStyle = getBarColor(names, index);
-    ctx.fillRect(x, COLUMN_BLOCK_Y - (height + GAP * 2), BAR_WIDTH, height);
-  };
-
-  /**
-   * функция вызывается, когда игрок проходит уровень.
-   * @param {Canvas} ctx - канвас на котором рисуется игра.
-   * @param {Array} names - массив, с именами игроков прошедших уровень. Имя самого игрока — Вы. Массив имён формируется случайным образом.
-   * @param {Array} times - массив, по длине совпадающий с массивом names. Массив содержит время прохождения уровня соответствующего игрока из массива names. Время прохождения уровня задано в миллисекундах.
-   */
-  window.renderStatistics = function (ctx, names, times) {
-
-    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR);
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-    writeText(ctx, 'Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3);
-    writeText(ctx, 'Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 5);
-
-    var maxTime = getMaxValue(times);
-
-    for (var i = 0; i < names.length; i++) {
-      var columnHeight = (BAR_HEIGHT * times[i].toFixed(0)) / maxTime;
-
-      writeText(ctx, times[i].toFixed(0), COLUMN_BLOCK_X + (BAR_GAP + BAR_WIDTH) * i, COLUMN_BLOCK_Y - (columnHeight + GAP * 3));
-      renderColumn(ctx, names, i, COLUMN_BLOCK_X + (BAR_GAP + BAR_WIDTH) * i, columnHeight);
-      writeText(ctx, names[i], COLUMN_BLOCK_X + (BAR_GAP + BAR_WIDTH) * i, COLUMN_BLOCK_Y);
+  for (var i = 1; i < items.length; i++) {
+    if (items[i] > maxItem) {
+      maxItem = items[i];
     }
-  };
+  }
 
-}());
+  return maxItem;
+};
+
+/**
+ * Функция, выбирающая случайное число из заданного промежутка.
+ * @param {number} minValue - минимальное число
+ * @param {number} maxValue - максимальное число
+ * @return {number} - случайное число из заданного промежутка,
+ * включая minValue и maxValue
+ */
+var getRandomNumber = function (minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+};
+
+/**
+ * Функция, генерирующая синий цвет случайной насыщенности.
+ * @return {string} - цвет в формате hsla
+ */
+var getRandomBlueColor = function () {
+  var saturation = getRandomNumber(MIN_SATURATION, MAX_SATURATION);
+  var lightness = getRandomNumber(MIN_LIGHTNESS, MAX_LIGHTNESS);
+
+  return 'hsla(240, ' + saturation + '%, ' + lightness + '%, 1)';
+};
+
+/**
+ * Функция, отрисовывающая гистограмму.
+ * @param {Object} ctx - контекст отрисовки
+ * @param {number} startX - x-координата левого верхнего угла гистограммы
+ * @param {number} startY - y-координата левого верхнего угла гистограммы
+ * @param {Array.<string>} labels - массив с подписями
+ * @param {Array.<number>} values - массив со значениями
+ */
+var renderBarChart = function (ctx, startX, startY, labels, values) {
+  var maxValue = findMaxItem(values);
+
+  for (var i = 0; i < values.length; i++) {
+    var barHeight = Math.round(BAR_MAX_HEIGHT * values[i] / maxValue);
+    var barX = startX + (BAR_WIDTH + BAR_MARGIN) * i;
+    var barY = startY + TEXT_HEIGHT + (BAR_MAX_HEIGHT - barHeight);
+
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.textBaseline = 'middle';
+    ctx.fillText(Math.round(values[i]), barX, barY - TEXT_HEIGHT / 2);
+    ctx.fillText(labels[i], barX, barY + barHeight + TEXT_HEIGHT / 2);
+
+    ctx.fillStyle = labels[i] === 'Вы' ? USER_BAR_COLOR : getRandomBlueColor();
+    ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
+  }
+};
+
+/**
+ * Отрисовка статистики.
+ * @param {Object} ctx - контекст отрисовки
+ * @param {Array.<string>} names - массив с именами игроков
+ * @param {Array.<number>} times - массив с результатами игроков, совпадающий по длине с массивом names
+ */
+window.renderStatistics = function (ctx, names, times) {
+  // Внутренние отступы
+  var cloudPaddingX = (CLOUD_WIDTH - (BAR_WIDTH + BAR_MARGIN) * names.length + BAR_MARGIN) / 2 - CLOUD_DEFLECTION;
+  var cloudPaddingY = (CLOUD_HEIGHT - CLOUD_DEFLECTION * 2 - TEXT_HEIGHT * 4 - BAR_MAX_HEIGHT) / 2;
+
+  // Координаты содержимого
+  var contentX = CLOUD_X + CLOUD_DEFLECTION + cloudPaddingX;
+  var contentY = CLOUD_Y + CLOUD_DEFLECTION + cloudPaddingY;
+
+  renderCloud(ctx, CLOUD_X + CLOUD_SHIFT, CLOUD_Y + CLOUD_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
+
+  ctx.font = CLOUD_TEXT_STYLE;
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.fillText('Ура вы победили!', contentX, contentY);
+  contentY += TEXT_HEIGHT;
+  ctx.fillText('Список результатов:', contentX, contentY);
+  contentY += TEXT_HEIGHT;
+
+  renderBarChart(ctx, contentX, contentY, names, times);
+};
